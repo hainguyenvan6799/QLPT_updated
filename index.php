0e4a5dce6ee643fa4d555ff38d3de04e2eb92fa3
Nguyen Van Hai <hainguyenvan6799>
2020-11-28 10:58:38 +0700

    update full

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
-<?php 
-	echo 'Hello';
- ?>
+<?php
+	require_once "./mvc/Bridge.php";
+	session_start();
+	$app = new App();
+	// $url = $_GET["url"];
+	// $arr = explode("/", $url);
+	// echo $arr[1];
+?>
