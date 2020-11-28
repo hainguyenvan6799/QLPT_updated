9a025d7e3460cb7715e98683b22901a4382a8c93
Nguyen Van Hai <hainguyenvan6799>
2020-11-28 12:14:49 +0700

    update full

--- mvc/core/App.php
+++ mvc/core/App.php
@@ -10,32 +10,31 @@
 			//Array ( [0] => Home [1] => SayHi [2] => 1 [3] => 2 [4] => 3 )
 			$arr = $this->UrlProcess();
 			//Xử lý controller
-			if(isset($arr))
-			{
-				if(file_exists("./mvc/Controllers/".$arr[0]."Controller.php"))
-				{
-					$this->controller = $arr[0];
-					unset($arr[0]); // xóa đi phần tử thứ 0 của mảng $arr
-				}
-				require_once "./mvc/Controllers/".$this->controller."Controller.php";
-				$homeobj = $this->controller . "Controller"; // Tên Controller
-				$this->controller = new $homeobj;
-				//Xử lý action
-				if(isset($arr[1]))
-				{
-					if(method_exists($this->controller, $arr[1]))
-					{
-						$this->action = $arr[1];
-					}
-					unset($arr[1]);
-				}
+			// if(isset($arr))
+			// {
+			// 	if(file_exists("./mvc/Controllers/".$arr[0]."Controller.php"))
+			// 	{
+			// 		$this->controller = $arr[0];
+			// 		unset($arr[0]); // xóa đi phần tử thứ 0 của mảng $arr
+			// 	}
+			// 	require_once "./mvc/Controllers/".$this->controller."Controller.php";
+			// 	$homeobj = $this->controller . "Controller"; // Tên Controller
+			// 	$this->controller = new $homeobj;
+			// 	//Xử lý action
+			// 	if(isset($arr[1]))
+			// 	{
+			// 		if(method_exists($this->controller, $arr[1]))
+			// 		{
+			// 			$this->action = $arr[1];
+			// 		}
+			// 		unset($arr[1]);
+			// 	}
 
-				//Xử lý Param
-				$this->param = $arr ? array_values($arr) : [];
-				// call_user_func_array([$this->controller, $this->action], $this->param);
-			}
-			echo $this->controller . '<br>';
-			echo $this->action . '<br>';
+			// 	//Xử lý Param
+			// 	$this->param = $arr ? array_values($arr) : [];
+			// 	call_user_func_array([$this->controller, $this->action], $this->param);
+			// }
+			
 		}
 
 		public function UrlProcess(){
@@ -49,6 +48,8 @@
 				require_once "./mvc/Controllers/".$this->controller."Controller.php";
 				$homeobj = $this->controller . "Controller"; // Tên Controller
 				$this->controller = new $homeobj;
+				echo $this->controller . '<br>';
+			echo $this->action . '<br>';
 				// call_user_func_array([$this->controller, $this->action], $this->param);
 			}
 		}
