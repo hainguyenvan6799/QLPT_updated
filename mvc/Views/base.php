a1829e064321dd7b76df8c4835501fbae1a3e95a
Nguyen Van Hai <hainguyenvan6799>
2020-11-28 20:42:38 +0700

    abc

--- mvc/Views/base.php
+++ mvc/Views/base.php
@@ -1,1 +1,2 @@
 <base href="https://quanlyphongtrostar.herokuapp.com/">
+<!-- <base href="http://localhost:88/QuanLyPhongTro/"> -->
--- mvc/Views/setup-link-and-script.php
+++ mvc/Views/setup-link-and-script.php
@@ -1,8 +1,8 @@
     <!-- <base href="http://localhost:88/QuanLyPhongTro/"> -->
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <!-- <script src="//code.jquery.com/jquery-1.11.1.min.js"></script> -->
 
     <link rel="stylesheet" type="text/css" href="public/css/chatbox.css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script> -->
 
     <!-- <script type="text/javascript" src="public/js/chatbox.js"></script> -->
 
@@ -11,7 +11,7 @@
     <link rel="stylesheet" type="text/css" href="public/css/app.css">
 
     <!-- Nhật -->
-   <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+   <!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
     <!-- Bootstrap-Core-CSS -->
 <!--     <link rel="stylesheet" href="public/css/style.css" type="text/css" media="all" />
  -->    <!-- Style-CSS -->
@@ -19,7 +19,7 @@
     <!-- <link href="public/css/font-awesome.css" rel="stylesheet"> -->
     <!-- //font-awesome-icons -->
     <!-- /Fonts -->
-    <link href="//fonts.googleapis.com/css?family=Hind:300,400,500,600,700" rel="stylesheet">
+    <!-- <link href="//fonts.googleapis.com/css?family=Hind:300,400,500,600,700" rel="stylesheet"> -->
     <!-- //Fonts -->
     <!-- Nhật -->   
     <!------ Include the above in your HEAD tag ---------->
