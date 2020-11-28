a697492972a3e5545c1a1d6e7138e089d1d7e3b9
Nguyen Van Hai <hainguyenvan6799>
2020-11-28 11:09:34 +0700

    update full

--- mvc/Bridge.php
+++ mvc/Bridge.php
@@ -1,9 +1,9 @@
 <?php
-// require_once "./mvc/Views/base.php";
-// 	require_once "./mvc/core/App.php";
-// 	require_once "./mvc/core/BaseController.php";
-// 	// cái này giống như middleware
-// 	require_once "./mvc/core/DB.php";
-// 	require_once "./mvc/core/vendor/autoload.php";
-	echo 'Hello';
+require_once "./mvc/Views/base.php";
+	require_once "./mvc/core/App.php";
+	require_once "./mvc/core/BaseController.php";
+	// cái này giống như middleware
+	require_once "./mvc/core/DB.php";
+	require_once "./mvc/core/vendor/autoload.php";
+	
 ?>
