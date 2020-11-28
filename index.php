07692ef39549c8300d398e41487f4c961abed1c7
Nguyen Van Hai <hainguyenvan6799>
2020-11-28 23:14:47 +0700

    abc

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	// $url = $_GET["url"];
 	// $arr = explode("/", $url);
 	// echo $arr[1];
-require_once 'mvc/core/vendor/autoload.php';
+require_once 'https://phongtrongoisao.herokuapp.com/mvc/core/vendor/autoload.php';
 $client = new MongoDB\Client(
     'mongodb+srv://hai:X2L3zGTavujQkwLW@phongtro.ezstc.mongodb.net/phongtrodb?retryWrites=true&w=majority');
 
