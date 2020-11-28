9fd03de4c46f9f6e2025d243ebc7ae493cb7f44b
Nguyen Van Hai <hainguyenvan6799>
2020-11-28 22:57:56 +0700

    abc

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	// echo $arr[1];
 require_once './mvc/core/vendor/autoload.php';
 $client = new MongoDB\Client(
-			    'mongodb+srv://hainguyenvan6799:Thu123456789@phongtro.ezstc.mongodb.net/phongtrodb?retryWrites=true&w=majority');
+    'mongodb+srv://hai:X2L3zGTavujQkwLW@cluster0.kyvzw.mongodb.net/phongtrodb?retryWrites=true&w=majority');
 
-			print_r($client->phongtrodb->users);
+$db = $client->test;
 ?>
