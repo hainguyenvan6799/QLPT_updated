14ced7bb17a5214178b58d769ea088011aef2be0
Nguyen Van Hai <hainguyenvan6799>
2020-11-28 20:45:36 +0700

    abc

--- mvc/Bridge.php
+++ mvc/Bridge.php
@@ -1,5 +1,5 @@
 <?php
-require_once "./mvc/Views/base.php";
+	require_once "./mvc/Views/base.php";
 	require_once "./mvc/core/App.php";
 	require_once "./mvc/core/BaseController.php";
 	// cái này giống như middleware
