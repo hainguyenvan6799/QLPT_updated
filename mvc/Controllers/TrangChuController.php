2a3e730fdd0b4df2e48cbb7a854d4ba5fc891aca
Nguyen Van Hai <hainguyenvan6799>
2020-11-28 16:47:08 +0700

    full update

--- mvc/Controllers/TrangChuController.php
+++ mvc/Controllers/TrangChuController.php
@@ -2,8 +2,16 @@
 	class TrangChuController extends BaseController
 	{
 		public function __construct(){
-			$a = $this->userModel;
-			$a->test();
+			$this->userModel = $this->model("User");
+			$this->Model = $this->model("PhongTro");
+			$this->Model1 = $this->model("ThietBi");
+			$this->messageModel = $this->model("Message");
+			$this->Model_phieuthu = $this->model("phieuthu");
+			$this->Model_phongtro = $this->model("PhongTro");
+			$this->Model_thietbi = $this->model("ThietBi");
+			$this->Model_khachthue = $this->model("KhachThue");
+			$this->Model_phieuthue = $this->model("PhieuThue");
+			$this->Model_phieutra = $this->model("PhieuTra");
 		}
 		public function Home(){
 			// $user_login = isset($_SESSION["user_id"]) ? $_SESSION["user_id"] : 'hello';
@@ -21,6 +29,7 @@
 			// print_r($getNoFriends);
 			
 			// echo "nguyen van hai";
+			$this->userModel->test();
 
 		}
 		public function XemChiTiet($maphong){
