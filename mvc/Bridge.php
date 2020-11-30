706d4835c8bd8b3d443b0405092b86c689b7d421
Nguyen Van Hai <hainguyenvan6799>
2020-11-30 11:46:40 +0700

    abc

--- mvc/Bridge.php
+++ mvc/Bridge.php
@@ -4,7 +4,7 @@
 	require_once "./mvc/core/App.php";
 	require_once "./mvc/core/BaseController.php";
 	// cái này giống như middleware
-	require_once "./mvc/core/DB.php";
+	// require_once "./mvc/core/DB.php";
 	
 	
 ?>
