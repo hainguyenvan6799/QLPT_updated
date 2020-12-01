04b4cb11e7d5226e9661d34a0ede2490df905664
Nguyen Van Hai <hainguyenvan6799>
2020-12-01 11:35:37 +0700

    abc

--- mvc/Views/ReloadEvery3s.php
+++ mvc/Views/ReloadEvery3s.php
@@ -1,1 +1,13 @@
-<?php echo "abc"; ?>
+<?php 
+	date_default_timezone_set('Asia/Ho_Chi_Minh');
+	$timestamp = strtotime(date("Y-m-d H:i:s"));
+	$time = $timestamp - 5;
+	$datetime = date("Y-m-d H:i:s", $time);
+
+	$message = new Message;
+    $usermodel = new User;
+    // $users = $usermodel->getFriendsOfUser($_SESSION["user_id"]);
+
+    // $userCollection = (new MongoDB\Client)->phongtrodb->users;
+ ?>
+  
