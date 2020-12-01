bd8ff837855d3204569680bd612cc6538a6a4849
Nguyen Van Hai <hainguyenvan6799>
2020-12-01 18:00:48 +0700

    bacs

--- mvc/Views/ReloadEvery3s.php
+++ mvc/Views/ReloadEvery3s.php
@@ -1,13 +1,3 @@
-<?php
-	date_default_timezone_set('Asia/Ho_Chi_Minh');
-	$timestamp = strtotime(date("Y-m-d H:i:s"));
-	$time = $timestamp - 5;
-	$datetime = date("Y-m-d H:i:s", $time);
-	$messageModel = $data["messageModel"];
-	$messageModel->test();
-    // $userModel = $data['userModel'];
-    // $users = $userModel->getFriendsOfUser($_SESSION["user_id"]);
-
-    // $userCollection = $data["userCollection"];
+<?php 
+	echo "tao met vl";
  ?>
- 
