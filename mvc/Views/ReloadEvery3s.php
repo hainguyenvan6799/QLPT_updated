f3f1995a52238c59e00543ff20b2b9100d195cde
Nguyen Van Hai <hainguyenvan6799>
2020-12-01 17:47:25 +0700

    bacs

--- mvc/Views/ReloadEvery3s.php
+++ mvc/Views/ReloadEvery3s.php
@@ -1,3 +1,12 @@
-<?php 
-	echo date("Y-m-d H:i:s");
+<?php
+	date_default_timezone_set('Asia/Ho_Chi_Minh');
+	$timestamp = strtotime(date("Y-m-d H:i:s"));
+	$time = $timestamp - 5;
+	$datetime = date("Y-m-d H:i:s", $time);
+	$messageModel = $data["messageModel"];
+    $userModel = $data['userModel'];
+    $users = $userModel->getFriendsOfUser($_SESSION["user_id"]);
+
+    $userCollection = $data["userCollection"];
  ?>
+ 
