e76fde64addb98223ba68d57552d1c8a7ab0b4f0
Nguyen Van Hai <hainguyenvan6799>
2020-12-01 07:09:40 +0700

    abc

--- index.php
+++ index.php
@@ -2,20 +2,4 @@
 	require_once "./mvc/Bridge.php";
 	session_start();
 	$app = new App();
-	// $url = $_GET["url"];
-	// $arr = explode("/", $url);
-	// echo $arr[1];
-// require_once './mvc/core/vendor/autoload.php';
-
-
-// $client = new MongoDB\Client(
-//     'mongodb+srv://hainguyenvan6799:FpStNIkhVebgmica@cluster0.kyvzw.mongodb.net/phongtrodb?retryWrites=true&w=majority');
-
-// $db = $client->phongtrodb;
-// 	// print_r($db->test);
-// 	$testde = $db->test->find();
-// 	foreach($testde as $t)
-// 	{
-// 		echo $t->ab;
-// 	}
 ?>
