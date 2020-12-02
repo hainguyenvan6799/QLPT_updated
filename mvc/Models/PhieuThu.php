730148b996032efbe3045458ffe19ee3bc6c28bf
Nguyen Van Hai <hainguyenvan6799>
2020-12-03 01:12:57 +0700

    abc

--- mvc/Models/PhieuThu.php
+++ mvc/Models/PhieuThu.php
@@ -36,14 +36,14 @@
 			}
 			$Content="Chao Anh/Chi ".$hoten.". Thong bao thu tien ngay ".$ngayGhi.". So dien ".$soDien."kW, gia ".$giaDien." VND/kW, so nuoc ".$soNuoc."khoi, gia ".$giaNuoc." VND/khoi, gia phong ".$giaPhong." VND/thang. Tong tien: ".$tongTien."VND.";
 			// require_once "./mvc/core/vendor/autoload.php";
-			$basic  = new \Nexmo\Client\Credentials\Basic('8815c793', 'ObpppFylQdD6h8Pz');
-			$client = new \Nexmo\Client($basic);
+			$basic  = new Nexmo\Client\Credentials\Basic('8815c793', 'ObpppFylQdD6h8Pz');
+			$client = new Nexmo\Client($basic);
 
 			$message = $client->message()->send([
 			    'to' =>'84' . $sdt,
 			    'from' => 'Vonage APIs',
 			    'text' => "acb",
-			    "timestamp" => date("Y-m-d H:i:s")
+			    "message-timestamp" => date("Y-m-d H:i:s")
 			]);
 
 
